e6ac89a1589b6760c8d2f7b80a6ecfe95602a918
bhesaniya dhruvin <bhesaniya902@example.com>
2022-11-30 19:16:31 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 5000;
+
+const static_path = path.join(__dirname, "../public/js");
+app.use(express.static(static_path));
+
+app.get("", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/about", (req, res) => {
+  res.send("wellcome to my aboutpage.");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("it's a contact side...");
+});
+
+app.get("*", (req, res) => {
+  res.send("404 error page was't found...");
+});
+
+app.listen(port, () => {
+  console.log("succsesfully...");
+});
